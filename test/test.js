ec8a41d2ab27c43e19ee133badf0540fb3947f9f
Yueyue07 <xww.qyy@gmail.com>
2016-01-03 15:47:09 -0800

    add the test example for the application of Chai and Mocha

--- test/test.js
+++ test/test.js
@@ -1,6 +1,25 @@
-var expect = require('chai').expect;
-describe ('A basic test', function() {
+var expect = require('chai').expect
+  , foo = 'bar'
+  , beverages = {tea: ['chai', 'motcha', 'oolong']};
+
+
+
+   expect(foo).to.equal('bar');
+
+   
+
+describe ('A basic test for string var foo', function() {
   it('should pass when everything is okay', function(){
-     expect(true).to.be.true;
+     expect(foo).to.equal('bar');
+     expect(foo).to.be.a('string');
+     expect(foo).to.have.length(3);
+  });
+});
+
+describe ('A basic test for object var beverages', function() {
+  it('should pass when everything is okay', function(){
+     expect(beverages).to.be.a('object');
+     expect(beverages).to.have.property('tea').with.length(3);
+
   });
 });
